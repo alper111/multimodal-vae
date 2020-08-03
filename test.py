339ae4b3d63bf7e2962acd065ad1e9ed2d9fbce9
Alper Ahmetoğlu <ahmetoglu.alper@gmail.com>
2020-08-03 16:14:00 +0300

    change test script for ur10 data

--- test.py
+++ test.py
@@ -1,132 +1,67 @@
-"""Test multimodal VAE and print Figure 4."""
-import os
-import sys
+"""Test multimodal VAE for UR10 data."""
+import argparse
+import yaml
 import torch
-import numpy as np
-import models
+import torchvision
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
+import numpy as np
+import models
+import data
 
-if torch.cuda.is_available():
-    device = "cuda"
-else:
-    device = "cpu"
+parser = argparse.ArgumentParser("Test multimodal VAE.")
+parser.add_argument("-opts", help="option file", type=str, required=True)
+args = parser.parse_args()
 
-if len(sys.argv) < 2:
-    print("Usage: python test.py <run_name>")
-    exit()
-run_name = sys.argv[1]
-out_path = os.path.join("out", run_name)
-if not os.path.exists(out_path):
-    os.makedirs(out_path)
+opts = yaml.safe_load(open(args.opts, "r"))
+print(yaml.dump(opts))
 
-x_test = torch.tensor(np.load("data/test.npy"), dtype=torch.float, device=device)
+testset = data.UR10Dataset("data", modality=["img", "joint"], action=["grasp", "move"], mode="test")
 
-in_blocks = [
-    [8, 40, 20],
-    [8, 40, 20],
-    [2, 10, 5],
-    [2, 10, 5],
-    [8, 40, 20]
-]
-out_blocks = [
-    [20, 40, 8, 16],
-    [20, 40, 8, 16],
-    [5, 10, 2, 4],
-    [5, 10, 2, 4],
-    [20, 40, 8, 16]
-]
-in_shared = [70, 100, 56]
-out_shared = [28, 100, 70]
-model = models.MultiVAE(in_blocks=in_blocks, in_shared=in_shared, out_shared=out_shared, out_blocks=out_blocks, init="xavier")
-model.to(device)
-model.load(os.path.join("save", run_name), "multivae.pt")
+model = models.MultiVAE(
+    in_blocks=opts["in_blocks"],
+    in_shared=opts["in_shared"],
+    out_shared=opts["out_shared"],
+    out_blocks=opts["out_blocks"],
+    init=opts["init_method"])
+model.to(opts["device"])
+model.load(opts["save"], "multivae_last")
 model.eval()
+print(model)
 
-x_prev = x_test.clone()
-x_vis = x_test.clone()
-x_prev[:, [0, 1, 2, 3, 8, 9, 10, 11, 16, 18, 20, 21, 22, 23]] = -2.0
-x_vis[:, :8] = -2.0
-x_vis[:, 16:] = -2.0
+traj_idx = np.random.randint(0, 20)
 
 with torch.no_grad():
-    _, _, y_prev_mu, y_prev_logstd = model(x_prev, sample=False)
-    _, _, y_vis_mu, y_vis_logstd = model(x_vis, sample=False)
-    y_prev_mu = y_prev_mu.cpu().numpy()
-    y_vis_mu = y_vis_mu.cpu().numpy()
-    y_prev_std = torch.exp(y_prev_logstd).cpu().numpy()
-    y_vis_std = torch.exp(y_vis_logstd).cpu().numpy()
+    x_img, x_joint = testset.get_trajectory(traj_idx)
+    N = x_img.shape[0]
+    start_idx = int(0.2 * N)
+    traj_length = N - start_idx
+    x_all = [x_img.to(opts["device"]), x_joint.to(opts["device"])]
+    x_partial = [x_img[start_idx:(start_idx+1)].to(opts["device"]), x_joint[start_idx:(start_idx+1)].to(opts["device"])]
+    # reconstruction
+    _, _, o_mu, o_logstd = model(x_all, sample=False)
+    img_m, joint_m = o_mu[0].cpu().clamp(-1.0, 1.0), o_mu[1].cpu()
+    img_s, joint_s = torch.exp(o_logstd[0]).cpu(), torch.exp(o_logstd[1]).cpu()
+    # forecasting
+    img_traj, joint_traj = model.forecast(x_partial, traj_length)
 
-x_test = x_test.cpu().numpy()
-fig, ax = plt.subplots(4, 1, figsize=(6, 6))
-for i in range(4):
-    ax[i].plot(x_test[:100, i], c="k")
-    ax[i].plot(y_prev_mu[:100, i], c="b")
-    ax[i].plot(y_vis_mu[:100, i], c="orange", linestyle="dashed")
-    lower_plot = y_vis_mu[:100, i] - y_vis_std[:100, i]
-    upper_plot = y_vis_mu[:100, i] + y_vis_std[:100, i]
-    ax[i].fill_between(np.arange(100), lower_plot, upper_plot, color="orange", alpha=0.5)
-    ax[i].set_ylabel("$q_%d$" % i)
-    ax[i].set_xlabel("Timesteps")
-pp = PdfPages(os.path.join(out_path, "q.pdf"))
+x_cat = torch.cat([x_img[:, :3], img_m[:, :3]], dim=3).permute(0, 2, 3, 1)
+torchvision.io.write_video("out/img_recons.mp4", ((x_cat/2+0.5)*255).byte(), fps=30)
+x_cat = torch.cat([x_img[start_idx:, 3:], img_traj[:, 3:].cpu()], dim=3).permute(0, 2, 3, 1)
+torchvision.io.write_video("out/img_forecast.mp4", ((x_cat/2+0.5)*255).byte(), fps=30)
+
+fig, ax = plt.subplots(3, 2)
+for i in range(3):
+    for j in range(2):
+        ax[i][j].plot(x_joint[:, i*2+j], c="k")
+        ax[i][j].plot(joint_m[:, i*2+j], c="orange", linestyle="dashed")
+        ax[i][j].plot(torch.arange(start_idx, N), joint_traj[:, i*2+j].cpu(), c="m", linestyle="dashed")
+        # lower_plot = joint_m[:, i*2+j] - joint_s[:, i*2+j]
+        # upper_plot = joint_m[:, i*2+j] + joint_s[:, i*2+j]
+        # ax[i][j].fill_between(np.arange(N), lower_plot, upper_plot, color="orange", alpha=0.5)
+        ax[i][j].set_ylabel("$q_%d$" % (i*2+j))
+        ax[i][j].set_xlabel("Timesteps")
+        ax[i][j].set_ylim(-1, 1)
+pp = PdfPages("out/joints_recons.pdf")
 pp.savefig(fig)
 pp.close()
-plt.close()
-
-fig, ax = plt.subplots(4, 1, figsize=(6, 6))
-for i in range(4):
-    ax[i].plot(x_test[:100, i+8], c="k")
-    ax[i].plot(y_prev_mu[:100, i+8], c="b")
-    ax[i].plot(y_vis_mu[:100, i+8], c="orange", linestyle="dashed")
-    lower_plot = y_vis_mu[:100, i+8] - y_vis_std[:100, i+8]
-    upper_plot = y_vis_mu[:100, i+8] + y_vis_std[:100, i+8]
-    ax[i].fill_between(np.arange(100), lower_plot, upper_plot, color="orange", alpha=0.5)
-    ax[i].set_ylabel("$v_%d$" % i)
-    ax[i].set_xlabel("Timesteps")
-pp = PdfPages(os.path.join(out_path, "v.pdf"))
-pp.savefig(fig)
-pp.close()
-plt.close()
-
-fig, ax = plt.subplots(4, 1, figsize=(6, 6))
-for i in range(4):
-    ax[i].plot(x_test[:100, i+20], c="k")
-    ax[i].plot(y_prev_mu[:100, i+20], c="b")
-    ax[i].plot(y_vis_mu[:100, i+20], c="orange", linestyle="dashed")
-    lower_plot = y_vis_mu[:100, i+20] - y_vis_std[:100, i+20]
-    upper_plot = y_vis_mu[:100, i+20] + y_vis_std[:100, i+20]
-    ax[i].fill_between(np.arange(100), lower_plot, upper_plot, color="orange", alpha=0.5)
-    ax[i].set_ylabel("$u_%d$" % i)
-    ax[i].set_xlabel("Timesteps")
-pp = PdfPages(os.path.join(out_path, "u.pdf"))
-pp.savefig(fig)
-pp.close()
-plt.close()
-
-plt.figure(figsize=(6, 2))
-plt.plot(x_test[:100, 16], c="k")
-plt.plot(y_prev_mu[:100, 16], c="b")
-plt.plot(y_vis_mu[:100, 16], c="orange", linestyle="dashed")
-lower_plot = y_vis_mu[:100, 16] - y_vis_std[:100, 16]
-upper_plot = y_vis_mu[:100, 16] + y_vis_std[:100, 16]
-plt.fill_between(np.arange(100), lower_plot, upper_plot, color="orange", alpha=0.5)
-plt.ylabel("Touch")
-plt.xlabel("Timesteps")
-pp = PdfPages(os.path.join(out_path, "p.pdf"))
-pp.savefig()
-pp.close()
-plt.close()
-
-plt.figure(figsize=(6, 2))
-plt.plot(x_test[:100, 18], c="k")
-plt.plot(y_prev_mu[:100, 18], c="b")
-plt.plot(y_vis_mu[:100, 18], c="orange", linestyle="dashed")
-lower_plot = y_vis_mu[:100, 18] - y_vis_std[:100, 18]
-upper_plot = y_vis_mu[:100, 18] + y_vis_std[:100, 18]
-plt.fill_between(np.arange(100), lower_plot, upper_plot, color="orange", alpha=0.5)
-plt.ylabel("Sound")
-plt.xlabel("Timesteps")
-pp = PdfPages(os.path.join(out_path, "s.pdf"))
-pp.savefig()
-pp.close()
-plt.close()
