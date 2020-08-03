577e293448551165ea847c51a48232a36bce4a0f
Alper Ahmetoğlu <ahmetoglu.alper@gmail.com>
2020-08-03 17:16:56 +0300

    noise, modality param, past future param

--- utils.py
+++ utils.py
@@ -12,27 +12,51 @@ def get_parameter_count(model):
     return total_num
 
 
-def noise_input(x, prob=0.5, bidirectional=False):
+def noise_input(x, prob=0.5, bidirectional=False, modality_noise=True):
+    N = x[0].shape[0]
+    D = len(x)
+    dev = x[0].device
+    if modality_noise:
+        temp = torch.distributions.Binomial(probs=0.5)
+        modality_mask = []
+        for i in range(D):
+            modality_mask.append(temp.sample((N, )).int().to(dev))
+            print(modality_mask[i])
+        global_mask = torch.ones(N, dtype=torch.int, device=dev)
+        for i in range(D):
+            global_mask *= (1 - modality_mask[i])
+        bad_idx = global_mask == 1
+        for i in range(D):
+            modality_mask[i][bad_idx] = 1
+            modality_mask[i] = modality_mask[i].unsqueeze(1).float()
+
     x_noised = []
     m = torch.distributions.Binomial(probs=prob)
-    alpha = m.sample((x[0].shape[0], ))
-    alpha = alpha.to(x[0].device)
-    r = torch.rand()
-    for i in range(len(x)):
+    alpha = m.sample((N, ))
+    alpha = alpha.to(dev)
+    for i in range(D):
         d = x[i].shape[1] // 2
-        x_noised.append(x[i].clone())
-        noise = torch.rand_like(x_noised[-1], device=x[i].device) * 2 - 1
-        if len(x[i].shape) == 4:
-            alpha = alpha.reshape(-1, 1, 1, 1)
-        else:
-            alpha = alpha.reshape(-1, 1)
-
         if bidirectional:
-            if r < 0.5:
-                x_noised[-1][:, d:] = alpha * x_noised[-1][:, d:] + (1-alpha) * (noise[:, d:])
-            else:
-                x_noised[-1][:, :d] = alpha * x_noised[-1][:, :d] + (1-alpha) * (noise[:, :d])
+            noise_mask = torch.ones(3, 2*d, device=dev)
+            noise_mask[1, :d] = 0.
+            noise_mask[2, d:] = 0.
         else:
-            x_noised[-1][:, d:] = alpha * x_noised[-1][:, d:] + (1-alpha) * (noise[:, d:])
+            noise_mask = torch.ones(2, 2*d, device=dev)
+            noise_mask[1, d:] = 0.
+
+        repeat_cnt = int(N // noise_mask.shape[0]) + 1
+        noise_mask = noise_mask.repeat(repeat_cnt, 1)[:N]
+
+        x_noised.append(x[i].clone())
+        # noise = torch.rand_like(x_noised[-1], device=dev) * 2 - 1
+        noise = torch.zeros_like(x_noised[-1], device=dev)
+        if len(x[i].shape) == 4:
+            noise_mask = noise_mask.unsqueeze(2).unsqueeze(3)
+            if modality_noise:
+                modality_mask[i] = modality_mask[i].unsqueeze(2).unsqueeze(3)
+
+        x_noised[-1] = (noise_mask * x_noised[-1] + (1-noise_mask) * noise)
+        if modality_noise:
+            x_noised[-1] = modality_mask[i] * x_noised[-1]
 
     return x_noised
