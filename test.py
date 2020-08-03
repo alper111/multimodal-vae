09a227b2ffe0078ff709ea950c95f745dadba9d9
Alper Ahmetoğlu <ahmetoglu.alper@gmail.com>
2020-08-04 02:49:01 +0300

    update test method

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 """Test multimodal VAE for UR10 data."""
+import os
 import argparse
 import yaml
 import torch
@@ -8,6 +9,7 @@ from matplotlib.backends.backend_pdf import PdfPages
 import numpy as np
 import models
 import data
+import utils
 
 parser = argparse.ArgumentParser("Test multimodal VAE.")
 parser.add_argument("-opts", help="option file", type=str, required=True)
@@ -26,42 +28,65 @@ model = models.MultiVAE(
     init=opts["init_method"])
 model.to(opts["device"])
 model.load(opts["save"], "multivae_last")
-model.eval()
+model.cpu().eval()
 print(model)
 
-traj_idx = np.random.randint(0, 20)
+out_folder = os.path.join(opts["save"], "outs")
+banned_mods = [1, 0]
+yje = torch.zeros(6)
+zje = torch.zeros(6)
 
-with torch.no_grad():
-    x_img, x_joint = testset.get_trajectory(traj_idx)
+for exp in range(20):
+    exp_folder = os.path.join(out_folder, str(exp))
+    if not os.path.exists(exp_folder):
+        os.makedirs(exp_folder)
+
+    x_img, x_joint = testset.get_trajectory(exp)
     N = x_img.shape[0]
-    start_idx = int(0.2 * N)
-    traj_length = N - start_idx
-    x_all = [x_img.to(opts["device"]), x_joint.to(opts["device"])]
-    x_partial = [x_img[start_idx:(start_idx+1)].to(opts["device"]), x_joint[start_idx:(start_idx+1)].to(opts["device"])]
-    # reconstruction
-    _, _, o_mu, o_logstd = model(x_all, sample=False)
-    img_m, joint_m = o_mu[0].cpu().clamp(-1.0, 1.0), o_mu[1].cpu()
-    img_s, joint_s = torch.exp(o_logstd[0]).cpu(), torch.exp(o_logstd[1]).cpu()
-    # forecasting
-    img_traj, joint_traj = model.forecast(x_partial, traj_length)
+    start_idx = int(0.5 * N)
+    forward_t = N - start_idx - 1
+    backward_t = start_idx
+    x_all = [x_img, x_joint]
+    xn_img, xn_joint = utils.noise_input(x_all, banned_mods, prob=[0., 1.], direction="forward", modality_noise=False)
+    x_condition = [xn_img[start_idx:(start_idx+1)], xn_joint[start_idx:(start_idx+1)]]
 
-x_cat = torch.cat([x_img[:, :3], img_m[:, :3]], dim=3).permute(0, 2, 3, 1)
-torchvision.io.write_video("out/img_recons.mp4", ((x_cat/2+0.5)*255).byte(), fps=30)
-x_cat = torch.cat([x_img[start_idx:, 3:], img_traj[:, 3:].cpu()], dim=3).permute(0, 2, 3, 1)
-torchvision.io.write_video("out/img_forecast.mp4", ((x_cat/2+0.5)*255).byte(), fps=30)
+    with torch.no_grad():
+        # one-step forward prediction
+        _, _, (y_img, y_joint), _ = model([xn_img, xn_joint], sample=False)
+        # forecasting
+        z_img, z_joint = model.forecast(x_condition, forward_t, backward_t, banned_modality=banned_mods)
 
-fig, ax = plt.subplots(3, 2)
-for i in range(3):
-    for j in range(2):
-        ax[i][j].plot(x_joint[:, i*2+j], c="k")
-        ax[i][j].plot(joint_m[:, i*2+j], c="orange", linestyle="dashed")
-        ax[i][j].plot(torch.arange(start_idx, N), joint_traj[:, i*2+j].cpu(), c="m", linestyle="dashed")
-        # lower_plot = joint_m[:, i*2+j] - joint_s[:, i*2+j]
-        # upper_plot = joint_m[:, i*2+j] + joint_s[:, i*2+j]
-        # ax[i][j].fill_between(np.arange(N), lower_plot, upper_plot, color="orange", alpha=0.5)
-        ax[i][j].set_ylabel("$q_%d$" % (i*2+j))
-        ax[i][j].set_xlabel("Timesteps")
-        ax[i][j].set_ylim(-1, 1)
-pp = PdfPages("out/joints_recons.pdf")
-pp.savefig(fig)
-pp.close()
+        y_img.clamp_(-1., 1.)
+        y_joint.clamp_(-1., 1.)
+        z_img.clamp_(-1., 1.)
+        z_joint.clamp_(-1, 1.)
+
+        fig, ax = plt.subplots(3, 2, figsize=(12, 10))
+        for i in range(3):
+            for j in range(2):
+                ax[i][j].plot(x_joint[:, i*2 + j + 6], c="k")
+                ax[i][j].plot(y_joint[:, i*2 + j + 6], c="b")
+                ax[i][j].plot(z_joint[:, i*2 + j + 6], c="m")
+                ax[i][j].scatter(start_idx, x_joint[start_idx, i*2 + j + 6], c="r", marker="x")
+                ax[i][j].set_ylabel("$q_%d$" % (i*2+j))
+                ax[i][j].set_xlabel("Timesteps")
+        pp = PdfPages(os.path.join(exp_folder, "joint-joints.pdf"))
+        pp.savefig(fig)
+        pp.close()
+
+        yje += ((x_joint[:, 6:] - y_joint[:, 6:])*3).abs().mean(dim=0)
+        zje += ((x_joint[:, 6:] - z_joint[:, 6:])*3).abs().mean(dim=0)
+
+        y_pixel_error = (utils.to_pixel(x_img[:, 3:]) - utils.to_pixel(y_img[:, 3:])).abs().mean()
+        z_pixel_error = (utils.to_pixel(x_img[:, 3:]) - utils.to_pixel(z_img[:, 3:])).abs().mean()
+        print("Exp: %d, onestep pixel error: %.4f, forecast pixel error: %.4f" % (exp, y_pixel_error, z_pixel_error))
+
+        x_cat = torch.cat([x_img[:, 3:], y_img[:, 3:]], dim=3).permute(0, 2, 3, 1)
+        torchvision.io.write_video(os.path.join(exp_folder, "joint-onestep.mp4"), utils.to_pixel(x_cat).byte(), fps=30)
+        x_cat = torch.cat([x_img[:, 3:], z_img[:, 3:]], dim=3).permute(0, 2, 3, 1)
+        torchvision.io.write_video(os.path.join(exp_folder, "joint-forecast.mp4"), utils.to_pixel(x_cat).byte(), fps=30)
+
+yje = np.degrees(yje/20)
+zje = np.degrees(zje/20)
+print("onestep joint errors: %.4f, %.4f, %.4f, %.4f, %.4f, %.4f" % (yje[0], yje[1], yje[2], yje[3], yje[4], yje[5]))
+print("forecast joint errors: %.4f, %.4f, %.4f, %.4f, %.4f, %.4f" % (zje[0], zje[1], zje[2], zje[3], zje[4], zje[5]))
