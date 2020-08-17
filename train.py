0ac55dcc43fbe402a594625032c71eec2ebad8ad
Alper Ahmetoğlu <ahmetoglu.alper@gmail.com>
2020-08-17 14:41:19 +0300

    update train script

--- train.py
+++ train.py
@@ -64,7 +64,6 @@ for e in range(opts["epoch"]):
                                      direction="both", modality_noise=True)
 
         loss = model.mse_loss(x_noised, x_all, lambd=opts["lambda"], beta=opts["beta"], sample=False, reduce=True)
-        # loss = model.loss(x_noised, x_all, lambd=opts["lambda"], beta=opts["beta"], sample=False)
         loss.backward()
         optimizer.step()
         running_avg += loss.item()
@@ -92,39 +91,44 @@ for e in range(opts["epoch"]):
             x_img, x_joint = valset.get_trajectory(np.random.randint(0, 1))
             N = x_img.shape[0]
             start_idx = int(0.5 * N)
-            traj_length = N - start_idx
+            forward_t = N - start_idx - 1
+            backward_t = start_idx
             x_all = [x_img.to(dev), x_joint.to(dev)]
-            x_partial = [x_img[start_idx:(start_idx+1)].to(dev), x_joint[start_idx:(start_idx+1)].to(dev)]
-            x_noised = utils.noise_input(x_all, banned_modality=[0, 0], prob=[0.0, 1.0], direction="forward")
-            # reconstruction
-            _, _, o_mu, o_logstd = model(x_all, sample=False)
-            _, _, noised_mu, _ = model(x_noised, sample=False)
-            img_m, joint_m = o_mu[0].cpu().clamp(-1., 1.), o_mu[1].cpu()
-            img_noised_m, joint_noised_m = o_mu[0].cpu().clamp(-1., 1.), o_mu[1].cpu()
-            img_s, joint_s = torch.exp(o_logstd[0]).cpu(), torch.exp(o_logstd[1]).cpu()
+            xn_img, xn_joint = utils.noise_input(x_all, banned_modality=[0, 0], prob=[0., 1.], direction="forward")
+            x_condition = [x_img[start_idx:(start_idx+1)].to(dev), x_joint[start_idx:(start_idx+1)].to(dev)]
+            x_condition[0][:, :3] = x_condition[0][:, 3:]
+            x_condition[1][:, :7] = x_condition[1][:, 7:]
+
+            # one-step forward prediction
+            _, _, (y_img, y_joint), _ = model([xn_img, xn_joint], sample=False)
             # forecasting
-            img_traj, joint_traj = model.forecast(x_partial, traj_length)
+            z_img, z_joint = model.forecast(x_condition, forward_t, backward_t, banned_modality=[0, 0])
 
-        x_cat = torch.cat([x_img[:, 3:], img_noised_m[:, 3:]], dim=3).permute(0, 2, 3, 1)
-        torchvision.io.write_video(os.path.join(opts["save"], "onestep_%d.mp4" % (e+1)),
-                                   utils.to_pixel(x_cat).byte(), fps=30)
-
-        x_cat = torch.cat([x_img[start_idx:, 3:], img_traj[:, 3:].cpu()], dim=3).permute(0, 2, 3, 1)
-        torchvision.io.write_video(os.path.join(opts["save"], "forecast_%d.mp4" % (e+1)),
-                                   utils.to_pixel(x_cat).byte(), fps=30)
+            y_img.clamp_(-1., 1.)
+            y_joint.clamp_(-1., 1.)
+            z_img.clamp_(-1., 1.)
+            z_joint.clamp_(-1, 1.)
 
         fig, ax = plt.subplots(3, 2)
         for i in range(3):
             for j in range(2):
-                ax[i][j].plot(x_joint[:, i*2+j], c="k")
-                ax[i][j].plot(joint_m[:, i*2+j], c="b")
-                ax[i][j].plot(torch.arange(start_idx, N), joint_traj[:, i*2+j].cpu(), c="m")
+                ax[i][j].plot(x_joint[:, i*2 + j + 7], c="k")
+                ax[i][j].plot(y_joint[:, i*2 + j + 7].cpu(), c="b")
+                ax[i][j].plot(z_joint[:, i*2 + j + 7].cpu(), c="m")
+                ax[i][j].scatter(start_idx, x_joint[start_idx, i*2 + j + 7], c="r", marker="x")
                 ax[i][j].set_ylabel("$q_%d$" % (i*2+j))
                 ax[i][j].set_xlabel("Timesteps")
         pp = PdfPages(os.path.join(opts["save"], "joints_recons%d.pdf" % (e+1)))
         pp.savefig(fig)
         pp.close()
 
-        del x_all[:], x_partial[:], x_noised[:], img_traj, joint_traj
+        x_cat = torch.cat([x_img[:, 3:], y_img[:, 3:].cpu()], dim=3).permute(0, 2, 3, 1)
+        torchvision.io.write_video(os.path.join(opts["save"], "onestep_%d.mp4" % (e+1)),
+                                   utils.to_pixel(x_cat).byte(), fps=30)
+        x_cat = torch.cat([x_img[:, 3:], z_img[:, 3:].cpu()], dim=3).permute(0, 2, 3, 1)
+        torchvision.io.write_video(os.path.join(opts["save"], "forecast_%d.mp4" % (e+1)),
+                                   utils.to_pixel(x_cat).byte(), fps=30)
+
+        del x_all[:], x_condition[:], z_img, z_joint
 
     model.save(opts["save"], "multivae_last")
