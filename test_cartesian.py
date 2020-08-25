f084f8154f05957e8184ff7f2acbe685a0db512e
Alper Ahmetoğlu <ahmetoglu.alper@gmail.com>
2020-08-25 14:24:16 +0300

    parameterize banned mods and prefix

--- test_cartesian.py
+++ test_cartesian.py
@@ -12,6 +12,8 @@ import utils
 
 parser = argparse.ArgumentParser("Test multimodal VAE.")
 parser.add_argument("-opts", help="option file", type=str, required=True)
+parser.add_argument("-banned", help="banned modalities", nargs="+", type=int, required=True)
+parser.add_argument("-prefix", help="output prefix", type=str, required=True)
 args = parser.parse_args()
 
 opts = yaml.safe_load(open(args.opts, "r"))
@@ -31,8 +33,6 @@ model.cpu().eval()
 print(model)
 
 out_folder = os.path.join(opts["save"], "outs")
-banned_mods = [0, 0]
-prefix = "both"
 yje = torch.zeros(7)
 zje = torch.zeros(7)
 ype = torch.zeros(7)
@@ -52,7 +52,7 @@ for exp in range(N):
     forward_t = L - start_idx - 1
     backward_t = start_idx
     x_all = [x_pos, x_joint]
-    xn_pos, xn_joint = utils.noise_input(x_all, banned_mods, prob=[0., 1.], direction="forward", modality_noise=False)
+    xn_pos, xn_joint = utils.noise_input(x_all, args.banned, prob=[0., 1.], direction="forward", modality_noise=False)
     x_condition = [x_pos[start_idx:(start_idx+1)], x_joint[start_idx:(start_idx+1)]]
     x_condition[0][:, 7:] = x_condition[0][:, :7]
     x_condition[1][:, 7:] = x_condition[1][:, :7]
@@ -61,7 +61,7 @@ for exp in range(N):
         # one-step forward prediction
         _, _, (y_pos, y_joint), _ = model([xn_pos, xn_joint], sample=False)
         # forecasting
-        z_pos, z_joint = model.forecast(x_condition, forward_t, backward_t, banned_modality=banned_mods)
+        z_pos, z_joint = model.forecast(x_condition, forward_t, backward_t, banned_modality=args.banned)
 
         y_pos.clamp_(-1., 1.)
         y_joint.clamp_(-1., 1.)
@@ -77,7 +77,7 @@ for exp in range(N):
                 ax[i][j].scatter(start_idx, x_joint[start_idx, i*2 + j] * 3, c="r", marker="x")
                 ax[i][j].set_ylabel("$q_%d$" % (i*2+j))
                 ax[i][j].set_xlabel("Timesteps")
-        pp = PdfPages(os.path.join(exp_folder, prefix+"-joints.pdf"))
+        pp = PdfPages(os.path.join(exp_folder, args.prefix+"-joints.pdf"))
         pp.savefig(fig)
         pp.close()
         plt.close()
@@ -95,7 +95,7 @@ for exp in range(N):
                 ax[j][i].scatter(start_idx, x_pos[start_idx, i*3 + j] * 3, c="r", marker="x")
                 ax[j][i].set_ylabel("$%s$" % mapper[i*3+j])
                 ax[j][i].set_xlabel("Timesteps")
-        pp = PdfPages(os.path.join(exp_folder, prefix+"-pos.pdf"))
+        pp = PdfPages(os.path.join(exp_folder, args.prefix+"-pos.pdf"))
         pp.savefig(fig)
         pp.close()
         plt.close()
@@ -106,11 +106,7 @@ yje = np.degrees(yje/N)
 zje = np.degrees(zje/N)
 ype = (ype/N)
 zpe = (zpe/N)
-print("onestep joint errors: %.4f, %.4f, %.4f, %.4f, %.4f, %.4f, %.4f" % (yje[0], yje[1], yje[2], yje[3], yje[4], yje[5], yje[6]))
-print("onestep joint errors: %.4f, %.4f, %.4f, %.4f, %.4f, %.4f, %.4f" % (yje[0], yje[1], yje[2], yje[3], yje[4], yje[5], yje[6]), file=open(os.path.join(out_folder, prefix+"-result.txt"), "a"))
-print("forecast joint errors: %.4f, %.4f, %.4f, %.4f, %.4f, %.4f, %.4f" % (zje[0], zje[1], zje[2], zje[3], zje[4], zje[5], zje[6]))
-print("forecast joint errors: %.4f, %.4f, %.4f, %.4f, %.4f, %.4f, %.4f" % (zje[0], zje[1], zje[2], zje[3], zje[4], zje[5], zje[6]), file=open(os.path.join(out_folder, prefix+"-result.txt"), "a"))
-print("onestep position errors: %.4f, %.4f, %.4f, %.4f, %.4f, %.4f" % (ype[0], ype[1], ype[2], ype[3], ype[4], ype[5]))
-print("onestep position errors: %.4f, %.4f, %.4f, %.4f, %.4f, %.4f" % (ype[0], ype[1], ype[2], ype[3], ype[4], ype[5]), file=open(os.path.join(out_folder, prefix+"-result.txt"), "a"))
-print("forecast position errors: %.4f, %.4f, %.4f, %.4f, %.4f, %.4f" % (zpe[0], zpe[1], zpe[2], zpe[3], zpe[4], zpe[5]))
-print("forecast position errors: %.4f, %.4f, %.4f, %.4f, %.4f, %.4f" % (zpe[0], zpe[1], zpe[2], zpe[3], zpe[4], zpe[5]), file=open(os.path.join(out_folder, prefix+"-result.txt"), "a"))
+print("onestep joint errors: %.4f, %.4f, %.4f, %.4f, %.4f, %.4f, %.4f" % (yje[0], yje[1], yje[2], yje[3], yje[4], yje[5], np.radians(yje[6])/30), file=open(os.path.join(out_folder, args.prefix+"-result.txt"), "a"))
+print("forecast joint errors: %.4f, %.4f, %.4f, %.4f, %.4f, %.4f, %.4f" % (zje[0], zje[1], zje[2], zje[3], zje[4], zje[5], np.radians(zje[6])/30), file=open(os.path.join(out_folder, args.prefix+"-result.txt"), "a"))
+print("onestep position errors: %.4f, %.4f, %.4f, %.4f, %.4f, %.4f" % (ype[0], ype[1], ype[2], ype[3], ype[4], ype[5]), file=open(os.path.join(out_folder, args.prefix+"-result.txt"), "a"))
+print("forecast position errors: %.4f, %.4f, %.4f, %.4f, %.4f, %.4f" % (zpe[0], zpe[1], zpe[2], zpe[3], zpe[4], zpe[5]), file=open(os.path.join(out_folder, args.prefix+"-result.txt"), "a"))
